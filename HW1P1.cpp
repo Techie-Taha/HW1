3b2b62ccaf222e3470dc7458830fc8b0517054b8
Taha <ta753090@gmail.com>
2026-03-06 11:59:40 -0500

    file added

---
+++ HW1P1.cpp
@@ -0,0 +1,17 @@
+/*
+
+
+
+What is the decimal-based value of the following constants?
+
+* 020 : decimal-based value is 16
+
+* 0X60 : decimal-based value is 96
+
+* 0XFE : decimal-based value is 254
+
+* 0XABC : decimal-based value is 2748
+
+
+
+*/
---
+++ HW1P2.cpp
@@ -0,0 +1,24 @@
+/*
+Write a C++ program that calculates sum of pair values above
+*/
+
+#include <iostream>
+using namespace std;
+int Sum(int X, int Y){
+
+    return X + Y;
+
+}
+
+int main(){
+    int result1, result2;
+
+    result1 = Sum(020, 0X60);
+    result2 = Sum(0XFE, 0XABC);
+
+    cout << "sum of pair values 020 and 0X60 are:  " << result1 << endl;
+    cout << "sum of pair values 0XFE and 0XABC are:  " << result2 << endl;
+    return 0;
+
+
+}
